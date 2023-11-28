e6146bdb60db69a553154d104c107bcf20572d81
Aman Sariya <amansariya@gmail.com>
2023-11-27 18:23:01 -0800

    Added all handjoints and successfully logged

--- Assets/Scripts/TrackingHands.cs
+++ Assets/Scripts/TrackingHands.cs
@@ -14,22 +14,44 @@ public class TrackingHands : MonoBehaviour
     [SerializeField] private OVRSkeleton skeletonRight;
 
     // Add all the relevant bone IDs
-    private readonly OVRSkeleton.BoneId[] FingertipIDs = new OVRSkeleton.BoneId[]
+    private readonly OVRSkeleton.BoneId[] handJoints = new OVRSkeleton.BoneId[]
     {
+        OVRSkeleton.BoneId.Hand_Start,
+        OVRSkeleton.BoneId.Hand_WristRoot,
+        OVRSkeleton.BoneId.Hand_ForearmStub,
+        OVRSkeleton.BoneId.Hand_Thumb0,
+        OVRSkeleton.BoneId.Hand_Thumb1,
+        OVRSkeleton.BoneId.Hand_Thumb2,
+        OVRSkeleton.BoneId.Hand_Thumb3,
+        OVRSkeleton.BoneId.Hand_Index1,
+        OVRSkeleton.BoneId.Hand_Index2,
+        OVRSkeleton.BoneId.Hand_Index3,
+        OVRSkeleton.BoneId.Hand_Middle1,
+        OVRSkeleton.BoneId.Hand_Middle2,
+        OVRSkeleton.BoneId.Hand_Middle3,
+        OVRSkeleton.BoneId.Hand_Ring1,
+        OVRSkeleton.BoneId.Hand_Ring2,
+        OVRSkeleton.BoneId.Hand_Ring3,
+        OVRSkeleton.BoneId.Hand_Pinky0,
+        OVRSkeleton.BoneId.Hand_Pinky1,
+        OVRSkeleton.BoneId.Hand_Pinky2,
+        OVRSkeleton.BoneId.Hand_Pinky3,
         OVRSkeleton.BoneId.Hand_ThumbTip,
         OVRSkeleton.BoneId.Hand_IndexTip,
         OVRSkeleton.BoneId.Hand_MiddleTip,
         OVRSkeleton.BoneId.Hand_RingTip,
         OVRSkeleton.BoneId.Hand_PinkyTip
+        //OVRSkeleton.BoneId.Hand_End
     };
 
     private void Start()
     {
-        StartCoroutine(OnOVRHandInitiallized());
+        StartCoroutine(OnOVRHandInitialized());
+        Debug.Log(Application.persistentDataPath);
     }
-    
+
     // Assigning the fingertipPrefab to the fingerTips
-    private IEnumerator OnOVRHandInitiallized()
+    private IEnumerator OnOVRHandInitialized()
     {
         while (hands[0].transform.childCount == 0 || hands[1].transform.childCount == 0)
             yield return new WaitForEndOfFrame();
@@ -37,7 +59,7 @@ public class TrackingHands : MonoBehaviour
         skeletonLeft = hands[0].GetComponentInChildren<OVRSkeleton>();
         skeletonRight = hands[1].GetComponentInChildren<OVRSkeleton>();
 
-        foreach (var bone in FingertipIDs)
+        foreach (var bone in handJoints)
         {
             var fingertipLeft = skeletonLeft.Bones.Single(a => a.Id == bone);
             var fingertipRight = skeletonRight.Bones.Single(a => a.Id == bone);
@@ -56,13 +78,18 @@ public class TrackingHands : MonoBehaviour
         foreach (var hand in hands)
         {
             OVRSkeleton skeleton = hand.GetComponentInChildren<OVRSkeleton>();
-            foreach (var boneID in FingertipIDs)
+            foreach (var boneID in handJoints)
             {
                 OVRBone bone = skeleton.Bones.FirstOrDefault(b => b.Id == boneID);
                 if (bone != null)
                 {
                     Transform boneTransform = bone.Transform;
                     logMessage += FormatTransform(boneTransform) + ",";
+                }
+                else
+                {
+                    //logMessage += FormatTransform
+                    Debug.LogWarning($"Bone not found: {boneID}");
                 }
             }
         }
